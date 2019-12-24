44f6618284a32c2997460bc738562cc7317d0232
jochen@homeland <no-go@localhost>
2019-12-24 12:43:58 +0100

    it is xmas time!

--- kerzeCode/kerzeCode.ino
+++ kerzeCode/kerzeCode.ino
@@ -1,24 +1,35 @@
-#define POTI A2
+// I use 8 LEDs with GND connected to ...
 
-int pins[4]  = {6,5,9,3};
-int flame[4] = {0,0,0,0};
-int modi = 0;
+int pins[8]    = {A3,A2,A1,A0,  13,12,11,10};
+//int pins[8]  = {10,11,12,13,  A0,A1,A2,A3};
+
+// ... and their + connected to a 68 Ohm resistor (and that connected to 3.3V)
+
+int flame[8] = {0,0,0,0,0,0,0,0}; // stores the LED blackout delay
 
 void setup() {
-  pinMode(POTI, INPUT);
-  for(int i=0; i<4; ++i) {
+  for(int i=0; i<8; ++i) {
     pinMode(pins[i], OUTPUT);
   }
 }
 
+int i=0;
 void loop() {
+  // floating the delay to through to LEDs
+  flame[7] = flame[6];
+  flame[6] = flame[5];
+  flame[5] = flame[4];
+  flame[4] = flame[3];
   flame[3] = flame[2];
   flame[2] = flame[1];
   flame[1] = flame[0];
-  flame[0] = random(0, 84);
-  modi = analogRead(POTI);
-  delay(random(50+(modi/10), 50+modi));
+  flame[0] = random(1, 25);
   
-  for (int i=0; i<4; ++i) analogWrite(pins[i], flame[i]);
+  digitalWrite(pins[i], HIGH);  // = OFF
+  delay(flame[i]);
+  digitalWrite(pins[i], LOW);   // = ON
+  delay(flame[i]);
+
+  i = (i+1)%8;
 }
 
